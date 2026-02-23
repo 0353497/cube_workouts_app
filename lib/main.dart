6b654943724da008fd393ecc37277608f1fe3ed4
Niek Geerligs <geerligsniek@gmail.com>
2026-02-23 15:10:15 +0100

    Refactor main function to initialize repository and set up BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:cube_workouts/core/routing/app_router.dart';
+import 'package:cube_workouts/data/repositories/local_workout_repository.dart';
+import 'package:cube_workouts/domain/bloc/workout_bloc.dart';
+import 'package:cube_workouts/domain/repositories/workout_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final WorkoutRepository repository = LocalWorkoutRepository();
+  await repository.init();
+
+  runApp(
+    BlocProvider(
+      create: (_) => WorkoutBloc(repository),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
