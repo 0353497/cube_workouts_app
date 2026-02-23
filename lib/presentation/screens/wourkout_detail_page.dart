4b68e3bdd21d901f235eb4294967ae8be262a9fa
Niek Geerligs <geerligsniek@gmail.com>
2026-02-23 11:40:54 +0100

    Add initial implementation of core structure with routing, themes, models, and UI components

---
+++ lib/presentation/screens/wourkout_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WourkoutDetailPage extends StatelessWidget {
+  const WourkoutDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
